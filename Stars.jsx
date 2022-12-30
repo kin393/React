ce879cbe9803c5e4c73eb4e5e00e43ec7fd59cb6
kin393 <34294833+kin393@users.noreply.github.com>
2022-12-30 17:29:27 -0500

    Update Stars.jsx

--- Stars.jsx
+++ Stars.jsx
@@ -9,23 +9,33 @@ const starStyle = {
   margin: "5px"
 };
 
+
 function Stars(props) {
   let star = document.querySelectorAll(".dot");
 
-  const [stars, SetStars] = useState(0);
 
+  const [rating, SetStars] = useState(0);
+
+  
   useEffect(() => {
     SetStars(props.rating);
-  });
+    for(let i = 0; i < star.length; i++){
+      star[i].classList.remove('gold')
+    }
+    for(let i = 0; i < rating; i++){
+      star[i].classList.add('gold')
+    }
+  },);
 
   return (
     <div>
+
       <h1>Rating</h1>
-      <div className="dot" style={starStyle}></div>
-      <div className="dot" style={starStyle}></div>
-      <div className="dot" style={starStyle}></div>
-      <div className="dot" style={starStyle}></div>
-      <div className="dot" style={starStyle}></div>
+      <div className="dot 1" style={starStyle}></div>
+      <div className="dot 2" style={starStyle}></div>
+      <div className="dot 3" style={starStyle}></div>
+      <div className="dot 4" style={starStyle}></div>
+      <div className="dot 5" style={starStyle}></div>
     </div>
   );
 }
